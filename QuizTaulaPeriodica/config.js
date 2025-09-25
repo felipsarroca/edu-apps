a95502f770fd1318116c7ce5a1d50072dc40fccb
felipsarroca <felip.sarroca@gmail.com>
2025-09-25 06:39:46 +0200

    Afegir tools

--- QuizTaulaPeriodica/config.js
+++ QuizTaulaPeriodica/config.js
@@ -1,1 +1,1 @@
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyt3vKG3i5nWcypKeUdXNYDh5zTr0PfWeMDj_HNwFu1jOrNIwgBQKDFWZd4Tw7uvWvg/exec";
+﻿export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyLbPNAH5h0DS5laa9fY8g6tekG_RUI7gv4NoGFnpj6Kabf7nczapJwe2stHq346_8z/exec";
