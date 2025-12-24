12df676b4b2c007347472c18ed3a23ac30bcea64
felipsarroca <felip.sarroca@gmail.com>
2025-12-24 11:06:47 +0100

    Update app.js

--- LesHores/app.js
+++ LesHores/app.js
@@ -636,13 +636,3 @@ function startConfetti() {
   if (confettiTimer) cancelAnimationFrame(confettiTimer);
   confettiTimer = requestAnimationFrame(draw);
 }
-
-function stopPendingLoad() {
-  if (document.readyState !== "complete") {
-    window.stop();
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(stopPendingLoad, 6000);
-});
